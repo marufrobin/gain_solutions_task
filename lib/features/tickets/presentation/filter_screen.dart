8ae5a5723ee7bab57e86e314c2940660cc4b5f56
marufrobin <marufrobin5@gmail.com>
2025-09-29 00:34:45 +0600

    feat: Implement tag search and apply button logic in filter screen
    - Added tag search functionality with TextField and clickable tags.
    - Refactored priority and brand filter widgets into separate methods.
    - Implemented logic to disable "Apply" button when no filters are selected.

--- lib/features/tickets/presentation/filter_screen.dart
+++ lib/features/tickets/presentation/filter_screen.dart
@@ -18,6 +18,16 @@ class _FilterScreenState extends State<FilterScreen> {
   TicketBrand? _selectedBrand;
   TicketPriority? _selectedPriority;
 
+  final _searchController = TextEditingController();
+
+  final List<String> _tags = ['tag one', 'tag two', 'tag three'];
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -32,14 +42,15 @@ class _FilterScreenState extends State<FilterScreen> {
         title: Text('Filters'),
         actions: [
           TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              foregroundColor: theme.colorScheme.onSurface,
-            ),
+            onPressed: _selectedBrand == null && _selectedPriority == null
+                ? null
+                : () {},
             child: Text(
               'Apply',
               style: theme.textTheme.bodyMedium?.copyWith(
-                color: theme.colorScheme.onSurface,
+                color: _selectedBrand == null && _selectedPriority == null
+                    ? theme.colorScheme.onSurface.withValues(alpha: 0.4)
+                    : theme.colorScheme.onSurface,
                 fontSize: 14,
                 fontWeight: FontWeight.w600,
               ),
@@ -72,53 +83,154 @@ class _FilterScreenState extends State<FilterScreen> {
                 fontWeight: FontWeight.w600,
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 8),
-              padding: const EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                color: theme.colorScheme.surface,
-                border: Border.all(
-                  color: theme.colorScheme.onSurface.withValues(alpha: 0.1),
-                ),
-                borderRadius: BorderRadius.circular(8),
+            _priorityDropDownWidget(theme),
+            const SizedBox(height: 16),
+            Text(
+              'Tags',
+              style: theme.textTheme.titleLarge?.copyWith(
+                color: theme.colorScheme.onSurface,
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
               ),
-              child: DropdownButton(
-                underline: SizedBox(),
-                icon: Icon(Icons.keyboard_arrow_down_rounded),
-                items: TicketPriority.values.map((e) {
-                  return DropdownMenuItem(
-                    value: e,
-                    child: Text(
-                      e.name,
-                      style: theme.textTheme.bodyMedium?.copyWith(
-                        color: theme.colorScheme.onSurface,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _searchController,
+              onChanged: (value) {
+                log(name: "value", value.isEmpty.toString());
+              },
+              onSubmitted: (value) {},
+              decoration: InputDecoration(
+                hintText: 'Search Contacts',
+                hintStyle: theme.textTheme.bodyMedium?.copyWith(
+                  fontSize: 16,
+                  color: theme.colorScheme.onSurface.withValues(alpha: 0.6),
+                ),
+                prefixIconConstraints: BoxConstraints(
+                  minHeight: 30,
+                  minWidth: 30,
+                ),
+                prefixIcon: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 16),
+                  child: SvgPicture.asset(
+                    AppAssetsPath.search,
+                    colorFilter: ColorFilter.mode(
+                      theme.colorScheme.onSurface.withValues(alpha: 0.6),
+                      BlendMode.srcIn,
                     ),
-                  );
-                }).toList(),
-                borderRadius: BorderRadius.circular(8),
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                isExpanded: true,
-                value: _selectedPriority,
-                hint: Text(
-                  'Select Priority',
-                  style: theme.textTheme.bodyMedium?.copyWith(
-                    color: theme.colorScheme.onSurface.withValues(alpha: 0.6),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
                   ),
                 ),
-                onChanged: (TicketPriority? value) {
-                  setState(() {
-                    _selectedPriority = value;
-                  });
-                },
+                fillColor: Color(0xffF3F4F8),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(100),
+                ),
               ),
+              style: theme.textTheme.bodyMedium?.copyWith(
+                fontSize: 16,
+                color: theme.colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Wrap(
+              spacing: 8,
+              children: _tags
+                  .map(
+                    (e) => _tagsWidget(
+                      theme: theme,
+                      title: e,
+                      onChanged: (value) {
+                        log(name: "value", value.toString());
+                        if (value != null && value.isNotEmpty) {
+                          setState(() {
+                            _searchController.text = value;
+                          });
+                        }
+                      },
+                    ),
+                  )
+                  .toList(),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container _tagsWidget({
+    required ThemeData theme,
+    required String title,
+    required Function(String?) onChanged,
+  }) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surface,
+        border: Border.all(
+          color: theme.colorScheme.onSurface.withValues(alpha: 0.1),
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            onChanged(title);
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Text(title),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container _priorityDropDownWidget(ThemeData theme) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surface,
+        border: Border.all(
+          color: theme.colorScheme.onSurface.withValues(alpha: 0.1),
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: DropdownButton(
+        underline: SizedBox(),
+        icon: Icon(Icons.keyboard_arrow_down_rounded),
+        items: TicketPriority.values.map((e) {
+          return DropdownMenuItem(
+            value: e,
+            child: Text(
+              e.name,
+              style: theme.textTheme.bodyMedium?.copyWith(
+                color: theme.colorScheme.onSurface,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          );
+        }).toList(),
+        borderRadius: BorderRadius.circular(8),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        isExpanded: true,
+        value: _selectedPriority,
+        hint: Text(
+          'Select Priority',
+          style: theme.textTheme.bodyMedium?.copyWith(
+            color: theme.colorScheme.onSurface.withValues(alpha: 0.6),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        onChanged: (TicketPriority? value) {
+          setState(() {
+            _selectedPriority = value;
+          });
+        },
       ),
     );
   }
